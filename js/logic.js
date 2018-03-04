d7d8289fdca64526b52a73d5b8d4d3db3d2c8af9
Mohammed Hameed <mohammed.hameed@email.com>
2018-03-03 20:21:02 -0600

    added history button functionality, added press enter to search functionality

--- js/logic.js
+++ js/logic.js
@@ -1,26 +1,56 @@
 $(document).ready(function(){
 
-    
+    var textBox;
 
-    $(".btn").on("click", function(){
+    $("#ajaxRequest").on("click", function(){
         $(".results").empty();
-        
-        var apiKey = "cnvVfq7XSIKMKEvBcaV7y9oFTHegmF6X",
-        search = $("input").val(),
-        resultCount = $(".dropdown option:selected").val(),
-        request = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=" + resultCount,
-        data; //store response data
+        textBox = $("input").val().trim(); //getting text box value
+        var history = $("<button class = 'ajaxHistory btn btn-success'>").val(textBox).html('<i class="fas fa-times-circle"></i> '+textBox);
+        $(".history").append(history);
+        $("input").val(""); //remove text from search box
+        //call ajax function and pass text box value
+        ajaxCall(textBox);
+    });
 
-        $.ajax({
+    // removes the history button when click on x
+    $(".history").on("click","i", function(){
+        $(".ajaxHistory").remove();
+    })
+
+    // runs ajax request from history buttons
+    $(".history").on("click",".ajaxHistory", function(){
+        $(".results").empty();
+        var historyBtn = $(this).val();
+        ajaxCall(historyBtn);
+    })
+
+    $("input").keyup(function(e){
+        if (e.keyCode === 13) {
+            $(".results").empty();
+        textBox = $("input").val().trim(); //getting text box value
+        var history = $("<button class = 'ajaxHistory btn btn-success'>").val(textBox).html('<i class="fas fa-times-circle"></i> '+textBox);
+        $(".history").append(history);
+        $("input").val(""); //remove text from search box
+        //call ajax function and pass text box value
+        ajaxCall(textBox);
+        };
+    })
+
+    function ajaxCall (search) {
+        var apiKey = "cnvVfq7XSIKMKEvBcaV7y9oFTHegmF6X",
+        resultCount = $(".dropdown option:selected").val(), //number of results
+        request = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=" + resultCount + "&rating=g";
+        
+        //ajax request 
+        $.ajax({ 
             method : "GET",
             url : request
-        }).then(function (res){
-            data = res;
+        }).then(function (res){ //response 
 
             // display data to document
             for (var i = 0; i < res.data.length; i++){
-                var col = $("<div class = 'img_box col-md-3 col-xs-6'>")
-                var image = $("<img>").attr ({
+                var col = $("<div class = 'img_box col-md-3 col-xs-6'>") //create div
+                var image = $("<img>").attr ({ //create image element with attrbutes
                     src : res.data[i].images.original.url,
                     alt : search
                 });
@@ -29,11 +59,7 @@ $(document).ready(function(){
             $(".results").append (col)
             }
         });
-
-    });
-    // var xhr = $.get();
-    // xhr.done(function(data) { console.log("success got data", data); });
-
+    }
 
 
 });
